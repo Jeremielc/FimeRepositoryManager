b594a887873115cc2439b6484004932c03b14abe
Jérémie Leclerc <jleclerc@ecole.ensicaen.fr>
2017-02-13 23:10:56 +0100

    Removed output directory.

--- Reman/src/com/fimelab/reman/controller/UploadPageController.java
+++ Reman/src/com/fimelab/reman/controller/UploadPageController.java
@@ -10,7 +10,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.*;
-import java.net.URI;
 
 @Path("/file")
 public class UploadPageController {
